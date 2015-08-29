a6b2cb5c8a3e4048e2631a740f00de3d0f0f9d1f
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-08-29 19:56:25 +0900

    Added Travis CI scripts.

---
+++ .travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in $(find . -type d -maxdepth 1); do
+    pushd ${i} > /dev/null 2>&1
+    if [ -f gradlew ]; then
+        echo "Build ${i}"
+        ./gradlew build
+    fi
+    popd > /dev/null 2>&1
+done
