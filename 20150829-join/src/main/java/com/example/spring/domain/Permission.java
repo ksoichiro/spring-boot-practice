73c638d60521f7a1df1329646d11b16545e79d49
Soichiro Kashima <soichiro.kashima@gmail.com>
2015-08-29 17:37:33 +0900

    Added ManyToMany join.

---
+++ 20150829-join/src/main/java/com/example/spring/domain/Permission.java
@@ -0,0 +1,21 @@
+package com.example.spring.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class Permission {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+
+    @OneToMany
+    @JoinTable(name = "RolePermission",
+            joinColumns = @JoinColumn(name = "permission_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+}
