afae39b3f7193c3ef59c23da88c6fb1f642e3396
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-02-21 18:38:14 +0900

    Add webResource plugin example

---
+++ 20160221-web-resource/src/main/java/com/example/spring/web/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
