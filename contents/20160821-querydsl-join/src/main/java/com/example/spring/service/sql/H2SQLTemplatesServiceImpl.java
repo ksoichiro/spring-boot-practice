83af4b6e835b3052388c6ff13331270920c72a02
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-08-21 23:45:58 +0900

    Fix class names

--- contents/20160821-querydsl-join/src/main/java/com/example/spring/service/sql/H2SQLTemplatesServiceImpl.java
+++ contents/20160821-querydsl-join/src/main/java/com/example/spring/service/sql/H2SQLTemplatesServiceImpl.java
@@ -1,15 +1,15 @@
 package com.example.spring.service.sql;
 
-import com.mysema.query.sql.MySQLTemplates;
+import com.mysema.query.sql.H2Templates;
 import com.mysema.query.sql.SQLTemplates;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
-@ConditionalOnProperty(name = "spring.datasource.platform", havingValue = "mysql")
+@ConditionalOnProperty(name = "spring.datasource.platform", havingValue = "h2", matchIfMissing = true)
 @Service
-public class MySQLSQLTemplatesServiceIml implements SQLTemplatesService {
+public class H2SQLTemplatesServiceImpl implements SQLTemplatesService {
     @Override
     public SQLTemplates getTemplates() {
-        return new MySQLTemplates();
+        return new H2Templates();
     }
 }
--- contents/20160821-querydsl-join/src/main/java/com/example/spring/service/sql/MySQLSQLTemplatesServiceImpl.java
+++ contents/20160821-querydsl-join/src/main/java/com/example/spring/service/sql/MySQLSQLTemplatesServiceImpl.java
@@ -1,15 +1,15 @@
 package com.example.spring.service.sql;
 
-import com.mysema.query.sql.H2Templates;
+import com.mysema.query.sql.MySQLTemplates;
 import com.mysema.query.sql.SQLTemplates;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
-@ConditionalOnProperty(name = "spring.datasource.platform", havingValue = "h2", matchIfMissing = true)
+@ConditionalOnProperty(name = "spring.datasource.platform", havingValue = "mysql")
 @Service
-public class H2SQLTemplatesServiceIml implements SQLTemplatesService {
+public class MySQLSQLTemplatesServiceImpl implements SQLTemplatesService {
     @Override
     public SQLTemplates getTemplates() {
-        return new H2Templates();
+        return new MySQLTemplates();
     }
 }
