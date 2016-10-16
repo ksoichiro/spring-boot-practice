831272de4c52722846c8ea989e05bbaf17ef7dfc
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-10-16 22:15:36 +0900

    Remove unused import

--- contents/20161016-logging/src/main/java/com/example/spring/web/ProjectController.java
+++ contents/20161016-logging/src/main/java/com/example/spring/web/ProjectController.java
@@ -6,7 +6,6 @@ import com.example.spring.util.LogHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
